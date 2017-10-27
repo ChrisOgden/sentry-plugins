320bdb24d51206f27c0838eb3769a5ff832de198
Christopher Ogden <13042897+ChrisOgden@users.noreply.github.com>
2017-10-26 22:33:52 -0400

    fix(bitbucket) Split IP range and single IPs
    Split the network of IPs and single IP list in order to pass validation tests

--- src/sentry_plugins/bitbucket/endpoints/webhook.py
+++ src/sentry_plugins/bitbucket/endpoints/webhook.py
@@ -22,7 +22,8 @@ logger = logging.getLogger('sentry.webhooks')
 
 # Bitbucket Cloud IP range:
 # https://confluence.atlassian.com/bitbucket/manage-webhooks-735643732.html#Managewebhooks-trigger_webhookTriggeringwebhooks
-BITBUCKET_IP_RANGE = [ipaddress.ip_network(u'104.192.143.0/24'), ipaddress.ip_address(u'34.198.203.127'), ipaddress.ip_address(u'34.198.178.64'), ipaddress.ip_address(u'34.198.32.85')]
+BITBUCKET_IP_RANGE = ipaddress.ip_network(u'104.192.143.0/24')
+BITBUCKET_IPS = [ipaddress.ip_address(u'34.198.203.127'), ipaddress.ip_address(u'34.198.178.64'), ipaddress.ip_address(u'34.198.32.85')]
 
 
 class Webhook(object):
@@ -150,7 +151,8 @@ class BitbucketWebhookEndpoint(View):
             return HttpResponse(status=204)
 
         if not ipaddress.ip_address(six.text_type(request.META['REMOTE_ADDR'])
-                                    ) in BITBUCKET_IP_RANGE:
+                                    ) in BITBUCKET_IP_RANGE or ipaddress.ip_address(six.text_type(request.META['REMOTE_ADDR'])
+                                    ) in BITBUCKET_IPs:
             logger.error(
                 'bitbucket.webhook.invalid-ip-range', extra={
                     'organization_id': organization.id,
